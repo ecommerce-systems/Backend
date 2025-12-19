0bb2eeccd9cb6d47503d65cd7b4ebad2e71aebe9
creepereye <iostream1204@gmail.com>
2025-12-19 20:13:49 +0900

    feat(prod): 서비스 컨테이너 이미지화
    - Dockerfile 작성으로 서비스 이미지 빌드 지원
    - Docker Compose 연동으로 멀티 서비스 실행 환경 구성

--- src/main/java/com/creepereye/ecommerce/global/config/RedisConfig.java
+++ src/main/java/com/creepereye/ecommerce/global/config/RedisConfig.java
@@ -1,8 +1,10 @@
 package com.creepereye.ecommerce.global.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -10,9 +12,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisHost, redisPort));
     }
 
     @Bean
