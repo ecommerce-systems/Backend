911f34b0beef4618b0e6eede911f689b45b86cce
creepereye <iostream1204@gmail.com>
2025-12-23 20:27:06 +0900

    feat(prod): CI 작성 -github action CI 작성 완료

--- src/test/java/com/creepereye/ecommerce/domain/auth/controller/AuthControllerSignupTest.java
+++ src/test/java/com/creepereye/ecommerce/domain/auth/controller/AuthControllerSignupTest.java
@@ -5,10 +5,14 @@ import com.creepereye.ecommerce.domain.user.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -18,6 +22,8 @@ import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@ActiveProfiles("local")
 class AuthControllerSignupTest {
 
     @Autowired
