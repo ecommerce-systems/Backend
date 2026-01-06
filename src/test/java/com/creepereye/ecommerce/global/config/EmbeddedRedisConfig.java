4b09cdaa6a606c260ebdcfeff8be369c352f629e
creepereye <iostream1204@gmail.com>
2026-01-06 23:44:02 +0900

    cors 도메인 추가

--- src/test/java/com/creepereye/ecommerce/global/config/EmbeddedRedisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.creepereye.ecommerce.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-@Configuration
-@Profile("dev")
-public class EmbeddedRedisConfig {
-}
